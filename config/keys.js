3954152e36c5d1069c0990eb5cf77b12d7d9a61f
jeevanandh <jeevavijay24@gmail.com>
2024-06-27 23:03:06 +0100

    db

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: process.env.ATLAS_URI || "mongodb+srv://jeevavijay24:<password>@ims.valllfv.mongodb.net/?retryWrites=true&w=majority&appName=IMS",
+    sessionSecret: process.env.SESSION_SECRET || "G2pHzDURnxUdS3ka"
+  };
