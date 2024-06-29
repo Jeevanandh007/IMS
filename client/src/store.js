c156ba4a6d19690e063bf540ffcf0e4e2d28b54d
jeevanandh <jeevavijay24@gmail.com>
2024-06-29 21:40:12 +0100

    debugging

--- client/src/store.js
+++ client/src/store.js
@@ -3,24 +3,5 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 
-const middleware = [thunk];
-function saveToLocalStore(state) {
-    try {
-      const serializedState = JSON.stringify(state);
-      localStorage.setItem("state", serializedState);
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
-  function loadFromLocalStore() {
-    try {
-      const serializedState = localStorage.getItem("state");
-      if (serializedState === null) return undefined;
-      return JSON.parse(serializedState);
-    } catch (error) {
-      console.log(error);
-      return undefined;
-    }
-  }
 export default store;
