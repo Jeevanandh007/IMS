49e17d0735f6224544996dfedf0b8095b273d38d
jeevanandh <jeevavijay24@gmail.com>
2024-06-29 21:49:05 +0100

    update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,33 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+const middleware = [thunk];
 
+function saveToLocalStore(state) {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem("state", serializedState);
+      } catch (error) {
+        console.log(error);
+      }
 
+    }
 
+    function loadFromLocalStore(){
+        try{
+            const serializedState=localStorage.getItem("state");
+            if(serializedState === null) return undefined;
+            return JSON.parse(serializedState);
+        }
+        catch(error){
+            console.log(error);
+            return undefined;
+        }
+        }
+    
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  saveToLocalStore(store.getState());
+});
 export default store;
