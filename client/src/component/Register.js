cbd00363314286acf4c8ade7fa23e09e2ad78deb
jeevanandh <jeevavijay24@gmail.com>
2024-06-30 01:04:50 +0100

    update

--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -16,4 +16,15 @@ const Register = () => {
       e.preventDefault();
       dispatch(register({ username, email, password }));
     };
-  
+
+    return (
+      auth.isAuthenticated
+      ? <Redirect to={{ pathname: '/login' }} />
+      : <div className='container mt-5' style={{ maxWidth: '400px' }}>
+        <h2 className='text-success'>Sign Up</h2>
+        <hr />
+        <form onSubmit={handleSubmit}></form>
+
+
+
+    export default Register;
