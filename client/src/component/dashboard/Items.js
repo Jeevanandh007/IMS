2c67fd26223c923d1070a3ff9d978e0db67aec24
jeevanandh <jeevavijay24@gmail.com>
2024-07-03 14:00:54 +0100

    items

--- client/src/component/dashboard/Items.js
+++ client/src/component/dashboard/Items.js
@@ -1,3 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
+import Table from './Table'
+import AddProduct from './AddProduct'
+import EditProduct from './EditProduct'
+
+
+export const Items = () => {
+    const product = useSelector(state => state.product)
+  
+
+    
+  return (
+    
+  )
+}
