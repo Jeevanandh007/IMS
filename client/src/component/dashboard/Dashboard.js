b8ae630b1fed7bae0a47bc56e8dcff1f0009d7e9
jeevanandh <jeevavijay24@gmail.com>
2024-07-03 13:25:04 +0100

    update

--- client/src/component/dashboard/Dashboard.js
+++ client/src/component/dashboard/Dashboard.js
@@ -16,4 +16,51 @@ const Dashboard = () => {
     
         return updated;
       }
-      
+
+      return auth.isAuthenticated ? (
+        <Router>
+            <div className='dashboard'>
+            <div className='sidebar'>
+            <div className='sidebar__welcome'>
+            Welcome, {makeFirstLetterCapital(auth.user.username)}!
+            </div>
+          <ul className='sidebar__list'>
+            <li className='sidebar__item'><Link className='sidebar__link' to='/dashboard'>
+              {/* <BoxImg fill="#eee" /> */}
+              <span>Items</span>
+            </Link></li>
+            <li className='sidebar__item'><Link className='sidebar__link' to='/dashboard/profile'>
+              <UserImg fill="#eee" />
+              <span>Profile</span>
+            </Link></li>
+            <li className='sidebar__item'><Link className='sidebar__link' to='/dashboard/settings'>
+              <CogImg fill="#eee" />
+              <span>Settings</span>
+            </Link></li>
+            <li className='sidebar__item'><button className='sidebar__logout' onClick={() => dispatch(logout())}>
+              <LogOutImg fill="#eee" />
+              <span>Log out</span>
+            </button></li>
+          </ul>
+          <div className="copyright">
+            &copy; 2024 Inventory Management App by Jeevanandh .
+          </div>
+        </div>
+        <div className="main">
+          <Switch>
+            <Route path='/dashboard/profile' render={() => (
+              <Profile
+                makeFirstLetterCapital={makeFirstLetterCapital} />
+            )} />
+            <Route path='/dashboard/settings' render={() => (
+              <Settings />
+            )} />
+            <Route path='/dashboard' render={() => <Items />} />
+          </Switch>
+        </div>
+
+            </div>
+    </Router>
+  ) : (<Redirect to={{ pathname: '/' }} />)
+}
+export default Dashboard
