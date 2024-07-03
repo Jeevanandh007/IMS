c1259cc03e03a08cb152ae22885f6613a27d0682
jeevanandh <jeevavijay24@gmail.com>
2024-07-03 15:52:26 +0100

    product mongo

--- routes/product.js
+++ routes/product.js
@@ -18,4 +18,30 @@ function checkAuthentication(req, res, next) {
   });
 
   //add product
+  // User registration
+  router.post('/add', (req, res) => {
+    
+          const  { name, qty, um, price, weight, description } = req.body;
+          const userId = req.user._id;
   
+          const newProduct = new Product({
+            name,
+            qty: Number(qty),
+            um,
+            price: Number(price),
+            weight: Number(weight),
+            description,
+            userId
+          });
+
+          newProduct.save()
+    .then(product => res.json(product))
+    .catch(error => res.status(400).json(`Error: ${error}`));
+});
+
+// Get product by ID
+     
+ 
+      module.exports =router;
+
+
