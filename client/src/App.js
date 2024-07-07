ce1f51bab2683814492d91d217cb8efd04ede588
Jeevanandh Vijayan <jeevavijay24@gmail.com>
2024-07-07 11:28:19 +0530

    added tost lib and reformatted

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import { Provider } from 'react-redux'
-import store from './store'
+import { Provider } from "react-redux";
+import store from "./store";
 
-import Home from './component/Home'
-import Login from './component/Login'
-import Register from './component/Register'
-import Dashboard from './component/dashboard/Dashboard'
+import Home from "./component/Home";
+import Login from "./component/Login";
+import Register from "./component/Register";
+import Dashboard from "./component/dashboard/Dashboard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Route path='/' exact component={Home} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-        <Route path='/dashboard' component={Dashboard} />
-      </Router>
-    </Provider>
-  )
-}
+    <div>
+      <Provider store={store}>
+        <Router>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Router>
+      </Provider>
+      <ToastContainer />
+    </div>
+  );
+};
 
-export default App
+export default App;
