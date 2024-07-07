4a62681bbe4bc3b11f41d613a39c791348bbc02c
Jeevanandh Vijayan <jeevavijay24@gmail.com>
2024-07-07 10:49:40 +0530

    resolved issues with router

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
 import store from './store'
 
 import Home from './component/Home'
 import Login from './component/Login'
-import Register from  './component/Register'
+import Register from './component/Register'
 import Dashboard from './component/dashboard/Dashboard'
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-        </Routes>
-        </Router>
+        <Route path='/' exact component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/dashboard' component={Dashboard} />
+      </Router>
     </Provider>
   )
 }
--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -1,73 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Link, Navigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { register } from '../actions/authActions';
-
+import { Link, Redirect } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { register } from "../actions/authActions";
 
 const Register = () => {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const auth = useSelector(state => state.auth);
-    const dispatch = useDispatch();
-  
-    const handleSubmit = e => {
-      e.preventDefault();
-      dispatch(register({ username, email, password }));
-    };
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-      auth.isAuthenticated
-      ? <Navigate to={{ pathname: '/login' }} />
-      : <div className='container mt-5' style={{ maxWidth: '400px' }}>
-        <h2 className='text-success'>Sign Up</h2>
-        <hr />
-        
-        <form onSubmit={handleSubmit}>
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(register({ username, email, password }));
+  };
 
+  return auth.isAuthenticated ? (
+    <Redirect to={{ pathname: "/login" }} />
+  ) : (
+    <div className="container mt-5" style={{ maxWidth: "400px" }}>
+      <h2 className="text-success">Sign Up</h2>
+      <hr />
+
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
-        <label htmlFor="username" className='text-primary'>Username</label>
-            <input
+          <label htmlFor="username" className="text-primary">
+            Username
+          </label>
+          <input
             type="text"
             name="username"
             className="form-control border-info"
             value={username}
-            onChange={e => setUsername(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
           />
-          </div>
+        </div>
 
-<div className="form-group">
-<label htmlFor="email" className='text-primary'>Email address</label>
-    <input
-    type="email"
-    name="email"
-    className="form-control border-info"
-    value={email}
-    onChange={e => setEmail(e.target.value)}
-  />
-  </div>
+        <div className="form-group">
+          <label htmlFor="email" className="text-primary">
+            Email address
+          </label>
+          <input
+            type="email"
+            name="email"
+            className="form-control border-info"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-<div className="form-group">
-<label htmlFor="password" className='text-primary'>Password</label>
-    <input
-    type="password"
-    name="password"
-    className="form-control border-info"
-    value={password}
-    onChange={e => setPassword(e.target.value)}
-  />
-</div>
+        <div className="form-group">
+          <label htmlFor="password" className="text-primary">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="form-control border-info"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-  <div className="button-container d-flex justify-content-between align-items-center mt-3">
-  <button type="submit" className="btn btn-primary">Register</button>
-   <Link to="/" className="btn btn-secondary ">Back to Home</Link>
-   </div>
-   </form>
-  </div>
- );
+        <div className="button-container d-flex justify-content-between align-items-center mt-3">
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
+          <Link to="/" className="btn btn-secondary ">
+            Back to Home
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
- 
+
 export default Register;
