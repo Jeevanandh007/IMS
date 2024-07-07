b7ef57a08dc2c528a38a595dba1770cac437fe59
Jeevanandh Vijayan <jeevavijay24@gmail.com>
2024-07-07 10:48:59 +0530

    resolved issues with store

--- client/src/store.js
+++ client/src/store.js
@@ -1,34 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import {thunk} from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-const middleware = [thunk];
 
-function saveToLocalStore(state) {
-    try {
-        const serializedState = JSON.stringify(state);
-        localStorage.setItem("state", serializedState);
-      } catch (error) {
-        console.log(error);
-      }
-
-    }
-
-    function loadFromLocalStore() {
-        try {
-            const serializedState=localStorage.getItem("state");
-            if(serializedState === null) return undefined;
-            return JSON.parse(serializedState);
-        }
-        catch(error){
-            console.log(error);
-            return undefined;
-        }
-        }
-        const persistedState =loadFromLocalStore();
-    
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-store.subscribe(() => {
-  saveToLocalStore(store.getState());
-});
 export default store;
