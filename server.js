82f7e10ade945d840b29a470e907d4eeee9b96a8
jeevanandh <jeevavijay24@gmail.com>
2024-07-24 00:47:09 +0100

    azure bug fix

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use("/api/products", checkAuthentication, productRouter);
 app.use("/api/users", userRouter);
 
 //azure deployement
-app.use(express.static(path.join(__dirname,'./client/build')));
+app.use(express.static('./client/build'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
